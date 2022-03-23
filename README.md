6b7111a54c33089bb97643924a4fcaf10cb316a8
neven.shalata <nevenh.1994@gmail.com>
2022-03-23 13:35:43 +0200

    My cv profile

---
+++ README.md
@@ -0,0 +1,26 @@
+# My C.V profile
+
+## hey I'm Neven shalata From israel , I'm a web developer .
+
+### in this project i fully explained about my Resume .
+
+### I've already documented about :
+- Personal information .
+- The languages I speak.
+- Several ways to communicate.
+- The programming languages I work in.
+- My hobbies
+- My education.
+- my career objective.
+- My EXPERIENCE.
+- My super skills.
+  
+ # install
+- please copy my http repository .
+- Go to any terminal.
+- put git clone and paste the link.
+- cd (name of repository).
+- code .
+  
+
+
---
+++ script/script.js
@@ -0,0 +1,16 @@
+// function Data(){
+//     let firstname=document.getElementById("firstname");
+//     let lastname=document.getElementById("lastname");
+//     let age=document.getElementById("age");
+
+//     alert( firstname.value + " "+ lastname.value+ " "  +age.value);
+// }
+
+function Data(){
+    let firstname=document.getElementById("firstname");
+    let lastname=document.getElementById("lastname");
+    let age=document.getElementById("age");
+    
+    alert( " Hello " +firstname.value+ " "+ lastname.value+ " " + " " + " age : "+ age.value);
+    
+    }
